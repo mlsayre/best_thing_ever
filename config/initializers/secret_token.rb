df90cb28717205ad2df1216c043515ea081806df
Matt Sayre <mlsayre@gmail.com>
2013-07-30 19:16:16 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BestThingEver::Application.config.secret_token = '943e91bf0483379a6e5888d6229a253abcdc05c704000c6d6250e3d00175e03464273ab7fabb1011057c415b561545885bcdd180bc3fa3d94ba49022e762afbb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+BestThingEver::Application.config.session_store :cookie_store, key: '_best_thing_ever_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# BestThingEver::Application.config.session_store :active_record_store
